4e9885a1002ac55e947fbfad692d2ca1e076f03b
Tiago Oliveira Santos <tiago.santos@somosquiq.com>
2023-02-11 17:24:33 -0300

    feat: Knowing HTTP status codes

--- src/server.js
+++ src/server.js
@@ -5,14 +5,21 @@ const users = [];
 const server = http.createServer((req, res) => {
   const { method, url } = req;
   if (method === 'GET') {
-    return res.end('Listagem de usuário')
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
-    return res.end('Criação de usuário')
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    })
+    return res.writeHead(201).end('Criação de usuário')
   }
 
-  return res.end("Hello World!");
+  return res.writeHead(404).end()
 })
 
 server.listen(3333);
